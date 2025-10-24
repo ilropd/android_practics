38cdb5bbd0682e5064e93ddbeb314f8fcf592ee1
Ilya Rozhkov <or.rsexpress@gmail.com>
2025-10-24 17:29:23 +0200

    Icons of status bar are white now

--- app/src/main/java/com/itb/postapp/MainActivity.kt
+++ app/src/main/java/com/itb/postapp/MainActivity.kt
@@ -1,9 +1,17 @@
 package com.itb.postapp
 
+import android.app.Activity
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.platform.LocalView
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsControllerCompat
 import androidx.navigation.compose.rememberNavController
 import com.itb.postapp.ui.navigation.AppNavHost
 import com.itb.postapp.ui.theme.PostAppTheme
@@ -14,9 +22,27 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             PostAppTheme {
+                SetupSystemBars()
                 val navController = rememberNavController()
                 AppNavHost(navController = navController)
             }
         }
     }
 }
+
+
+@Composable
+fun SetupSystemBars() {
+    val view = LocalView.current
+    val window = (view.context as Activity).window
+
+    SideEffect {
+        window.statusBarColor = Color.Transparent.toArgb()
+
+        WindowInsetsControllerCompat(window, window.decorView).apply {
+            isAppearanceLightStatusBars = false
+            isAppearanceLightNavigationBars = false
+        }
+    }
+}
+
